510ffd62df75e5ba1404a9e13f7c7ff9b26c5f11
oubaoyao <673411498@qq.com>
2021-03-30 14:32:48 +0800

    添加了倒计时功能

--- Assets/Projects/Scripts/Frame/UI/Panel/ARPanel/ARPanel.cs
+++ Assets/Projects/Scripts/Frame/UI/Panel/ARPanel/ARPanel.cs
@@ -7,6 +7,11 @@ using UnityEngine.UI;
 public class ARPanel : BasePanel
 {
     public Button button_yandi, button_huangdi, button_fuxi;
+
+    private float BackTime = 5;
+    private float Back_Time;
+    private bool IsBack;
+
     public override void InitFind()
     {
         base.InitFind();
@@ -23,7 +28,6 @@ public class ARPanel : BasePanel
         });
 
         button_huangdi.onClick.AddListener(() => {
-            Debug.Log("打开黄帝");
             ARState.SwitchPanel(PanelName.HuangdiPanel);
         });
 
@@ -31,5 +35,32 @@ public class ARPanel : BasePanel
             ARState.SwitchPanel(PanelName.FuxiPanel);
         });
     }
+
+    public override void Open()
+    {
+        base.Open();
+        IsBack = true;
+        Back_Time = BackTime;
+    }
+
+    private void Update()
+    {
+        if (Back_Time > 0 && IsBack)
+        {
+            Back_Time -= Time.deltaTime;
+            //LogMsg.Instance.Log(Back_Time.ToString());
+            if (Back_Time <= 0)
+            {
+                IsBack = false;
+                ARState.SwitchPanel(PanelName.WaitPanel);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Back_Time = BackTime;
+            }
+        }
+    }
+
 
 }
